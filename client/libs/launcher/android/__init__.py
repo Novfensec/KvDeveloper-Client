3e5ce399432df460b6b1faf59d90863888f2293f
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-07-09 20:29:58 +0530

    Update __init__.py

--- client/libs/launcher/android/__init__.py
+++ client/libs/launcher/android/__init__.py
@@ -1,22 +1,24 @@
 import os
-
 from jnius import autoclass
+from android.runnable import run_on_ui_thread
 
 PythonActivity = autoclass("org.kivy.android.PythonActivity")
 ClientActivity = autoclass("org.kvdeveloper.client.ClientActivity")
-
 Intent = autoclass("android.content.Intent")
 String = autoclass("java.lang.String")
 
 activity = PythonActivity.mActivity
-
 AppStorageDir = os.path.join(activity.getFilesDir().getAbsolutePath(), "Applications")
 
+@run_on_ui_thread
 def launch_client_activity(entrypoint_path: str, *args) -> None:
-    intent = Intent(activity, ClientActivity)
+    intent = Intent(activity, ClientActivity.class_)
     intent.putExtra("entrypoint", String(entrypoint_path))
-
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     activity.startActivity(intent)
 
+@run_on_ui_thread
 def finish_client_activity(*args) -> None:
-    ClientActivity.finish()
+    current_activity = PythonActivity.mActivity
+    if isinstance(current_activity, ClientActivity):
+        current_activity.finish()
