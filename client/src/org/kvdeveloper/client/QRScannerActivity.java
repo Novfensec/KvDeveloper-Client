8602929cd887e76a0deec53a62416e7346700f94
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-07-24 18:13:30 +0530

    Update QRScannerActivity.java

--- client/src/org/kvdeveloper/client/QRScannerActivity.java
+++ client/src/org/kvdeveloper/client/QRScannerActivity.java
@@ -13,7 +13,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.common.InputImage;
